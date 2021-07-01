1dcb0d62286cf97929dcc3577c5f10b7c6be8b11
Smirnov224 <86654997+Smirnov224@users.noreply.github.com>
2021-07-01 09:04:41 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -6,3 +6,11 @@ import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+
+Почему?
+Traceback (most recent call last):
+  File "C:/Users/Пользователь/Desktop/PYTHON/Проги/random.py", line 6, in <module>
+    import random
+  File "C:\Users\Пользователь\Desktop\PYTHON\Проги\random.py", line 8, in <module>
+    print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
+AttributeError: partially initialized module 'random' has no attribute 'randint' (most likely due to a circular import)
